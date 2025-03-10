9331f9e0b323e76c6d1e35803e861289033f28e0
cuzimapanda2003 <marcblais03@gmail.com>
2025-03-10 16:20:51 -0400

    Update labo.ino

--- labo/labo.ino
+++ labo/labo.ino
@@ -4,15 +4,26 @@ LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change acc
 
 unsigned long currentTime = 0;
 
-uint8_t anchor[8] = {
-  0b01110,
-  0b01010,
-  0b01110,
+uint8_t chiffre[8] = {
+  0b11100,
   0b00100,
-  0b10101,
-  0b10101,
-  0b01110,
-  0b00100
+  0b11100,
+  0b00100,
+  0b11100,
+  0b00111,
+  0b00001,
+  0b00001
+};
+uint8_t anchor[8] =
+{
+    0b01110,
+    0b01010,
+    0b01110,
+    0b00100,
+    0b10101,
+    0b10101,
+    0b01110,
+    0b00100
 };
 
 
@@ -20,25 +31,33 @@ uint8_t anchor[8] = {
 
 void setup() {
   Serial.begin(115200);
-
-  lcd.begin();
-  lcd.backlight();
+start();
 
 
 
 
+}
 
 void loop() {
   currentTime = millis();
 }
 
+void start(){
 
-void debut(){
-int i = 0;
-if(i == 0){
-  
+  lcd.begin();
+  lcd.backlight();
+  lcd.createChar(2, chiffre);
+   lcd.createChar(3, anchor);
+  lcd.print("Blais");
+  lcd.setCursor(0,1);
+  lcd.write(3);
+   lcd.write(2);
+
+  delay(3000);
+
 }
 
-}
+
+
 
 
